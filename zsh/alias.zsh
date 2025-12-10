17466f59c572ad4c45b4d075115d4cf6321ceddc
Tzero Ocne <tzeroocne.interface@gmail.com>
2025-12-10 14:48:04 +0700

    feat(alias): add tree alias
    - also move alias definition to separate file

---
+++ zsh/alias.zsh
@@ -0,0 +1,16 @@
+alias oil='nvim -c "Oil"'
+alias bare="NVIM_APPNAME=nvim-bare nvim"
+alias nvim-lazy="NVIM_APPNAME=nvim-lazy nvim"
+alias lazy=nvim-lazy
+alias nvim-mini="NVIM_APPNAME=nvim-mini nvim"
+alias mini=nvim-mini
+alias dadbod='nvim -c "DBUI"'
+alias nvq='nvim -c "q"'
+alias rmshada='rm -rf $LOCALAPPDATA/nvim*data/shada/*'
+alias rmswap='rm -rf $LOCALAPPDATA/nvim*data/swap/*'
+alias lg='lazygit'
+alias gim='git init;git branch -M main;bannerDone'
+alias els='els.zsh'
+alias els-cli='els-cli.zsh'
+alias els-env='els-env.zsh'
+alias tree='tree.com'
--- zsh/init.zsh
+++ zsh/init.zsh
@@ -9,21 +9,7 @@ done
 
 HISTFILE=~/.config/zsh/history/.zsh_history
 
-alias oil='nvim -c "Oil"'
-alias bare="NVIM_APPNAME=nvim-bare nvim"
-alias nvim-lazy="NVIM_APPNAME=nvim-lazy nvim"
-alias lazy=nvim-lazy
-alias nvim-mini="NVIM_APPNAME=nvim-mini nvim"
-alias mini=nvim-mini
-alias dadbod='nvim -c "DBUI"'
-alias nvq='nvim -c "q"'
-alias rmshada='rm -rf $LOCALAPPDATA/nvim*data/shada/*'
-alias rmswap='rm -rf $LOCALAPPDATA/nvim*data/swap/*'
-alias lg='lazygit'
-alias gim='git init;git branch -M main;bannerDone'
-alias els='els.zsh'
-alias els-cli='els-cli.zsh'
-alias els-env='els-env.zsh'
+. $HOME/.config/nnry/zsh/alias.zsh
 
 # Function to bind arrow keys outside of menuselect
 bindArrowKeys() {
